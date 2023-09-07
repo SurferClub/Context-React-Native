a02e292e2b3401f1cc52a49ae512d93b6538c9d6
SurferClub <harley98.01@hotmail.com>
2023-09-07 13:41:15 -0500

    mejorando estilos

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import {ContextProvider, Context} from './Global/Context'  
@@ -11,25 +11,36 @@ function HomeScreen({navigation}) {
   const {val,setVal,val1,setVal1} = useContext(Context)
 
   return (
-    <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      
+      <Text style={styles.containerText}>
+        HomeScreen</Text>
+      
       <Text>{val}</Text>
       <Text>{val1}</Text>
+      <View style={{flexDirection: 'row'}}>
       <Button title="otra pantalla"
       onPress={()=> navigation.navigate('OtraPantalla')}
       />
-      <Button title='Incremento' onPress={()=>{
-        setVal(val + 1)
-        setVal1(val1 + 1)
-      }}/>
+      <Button 
+         color="#f194ff" 
+        title='Incremento' 
+        onPress={()=>{
+          setVal(val + 1)
+          setVal1(val1 + 1)
+        }}/>
+      </View>
     </View>
   )
 }
 
 function OtraPantalla({navigation}) {
+  const {val,setVal,val1,setVal1} = useContext(Context)
   return (
     <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
       <Text>HomeScreen</Text>
+      <Text>{val}</Text>
+      <Text>{val1}</Text>
       <Button title="Go back"
       onPress={()=> navigation.goBack()}
       >
@@ -60,6 +71,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
+  containerText:{
+    fontSize: 22,
+    margin:20,
+    paddingTop:170,
+  }
+  ,
+  button:{
+    paddingVertical: 22,
+    borderRadius: 4,
+    backgroundColor: 'black',
+    alignItems: 'space-between',
+    backgroundColor: 'black',
+  }
 });
